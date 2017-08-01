ded7f5428ef9f547b9f3e313cb2808f581248df8
xzakm3 <zakyz.martin@gmail.com>
2017-08-01 16:05:40 +0200

    styling layout

--- app/controllers/turkey_data_controller.rb
+++ app/controllers/turkey_data_controller.rb
@@ -4,21 +4,46 @@ class TurkeyDataController < ApplicationController
            "podhorany" => 6,
            "horne_saliby" => 6,
            "cabaj" => 14,
-           "mala_stefanovicova" => 6
+           "mala_stefanovicova" => 6,
+           "test" => 2
   }
   def new
-    farm = chopfirst(request.fullpath.)
-    @halls = FARMS[farm];
+    get_halls_number(request.fullpath)
+
     render 'new';
   end
 
   def create
+    prev_url = params[:farm]
+    get_halls_number(params[:farm])
+    turkey_death_data = params[:death]
+    turkey_hatch_data = params[:hatch]
+
+
+    turkey_death_data.each_with_index do |(key, value), index|
+      break if index == @halls
+      puts(key)
+      puts(value)
+    end
+
+    turkey_hatch_data.each_with_index do |(key, value), index|
+      break if index == @halls
+      puts(key)
+      puts(value)
+    end
+
+    redirect_to(prev_url)
   end
 
   private
 
     def chopfirst(text)
       text[1..-1]
+    end
+
+    def get_halls_number(raw_string_farm)
+      farm = chopfirst(raw_string_farm)
+      @halls = FARMS[farm]
     end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'turkey_data#test'
+  root 'static_pages#home'
+  get '/home', to: 'static_pages#home'
   get '/stefanovicova', to: 'turkey_data#new'
   get '/borovce', to: 'turkey_data#new'
   get '/cabaj', to: 'turkey_data#new'
   get '/horne_saliby', to: 'turkey_data#new'
   get '/podhorany', to: 'turkey_data#new'
   get '/mala_stefanovicova', to: 'turkey_data#new'
-  post '/test', to: 'turkey_data#create'
+  get '/test', to: 'turkey_data#new'
+  get  '/signup',  to: 'users#new'
+  post '/turkey_data', to: 'turkey_data#create'
 end
