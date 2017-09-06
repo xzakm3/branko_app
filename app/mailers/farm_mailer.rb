b3923e7f724343aeae5e440f94346110a8683c83
xzakm3 <zakyz.martin@gmail.com>
2017-09-06 16:07:33 +0200

    mail form done + added comments to user form and mail form

--- app/mailers/farm_mailer.rb
+++ app/mailers/farm_mailer.rb
@@ -9,6 +9,7 @@ class FarmMailer < ApplicationMailer
     @deaths = death_info
     @hatches = hatch_info
     @user = user
+    attachments.inline["logo.png"] = File.read("#{Rails.root}/app/assets/images/logo2.png")
     mail to: "zakyz.martin@gmail.com", subject: "[Farma #{user.farm.name}] - INFO ", from: user.email
   end
 end
